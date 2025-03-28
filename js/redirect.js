3261e74b5dd7fdde9d226ae206a701062f7a0b0b
duncangreene <duncan.greene@columbia.edu>
2025-03-28 18:30:06 -0400

    test

--- js/redirect.js
+++ js/redirect.js
@@ -1,14 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const redirect = urlParams.get('redirect');
-          
+    const redirect = sessionStorage.getItem('redirect');
     if (redirect) {
-        // Force a hard navigation
-        window.location.replace(redirect);
-    } else {
-        // Initialize normal SPA routing
-        if (window.handleNavigation) {
-            window.handleNavigation(window.location.pathname);
-        }
+      sessionStorage.removeItem('redirect');
+      history.replaceState({}, '', redirect);
+      window.handleNavigation(redirect); // Now works!
     }
-});
+  });
