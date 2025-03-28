07391913baf2b43ce3cf1027ca33c373e7a67a19
duncangreene <duncan.greene@columbia.edu>
2025-03-28 19:03:04 -0400

    basic auth change

--- js/auth.js
+++ js/auth.js
@@ -3,7 +3,7 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstati
 
 const firebaseConfig = {
     apiKey: "AIzaSyAOSxXEAlWwuvWqWbNCVDBSjvFepVgn8jc",
-    authDomain: "qp-auth-d9e52.firebaseapp.com",
+    authDomain: "quantapus.com",
     projectId: "qp-auth-d9e52",
     storageBucket: "qp-auth-d9e52.firebasestorage.app",
     messagingSenderId: "119036334637",
